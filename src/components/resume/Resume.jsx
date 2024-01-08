04825e0050bb86118ab696bdbbeaba30f24a4413
TheTwin96 <annielvallevalera96@outlook.com>
2024-01-08 12:56:30 -0600

    Changes.

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -8,13 +8,24 @@
 //   )
 // }
 
+// import React from 'react'
+// import PDF from '../../assets/Anniel Valle Valera.pdf'
+
+// export const Resume = () => {
+//   return (
+//     <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+//       <iframe src={PDF} width='100%' height='100%' />
+//     </div>
+//   )
+// }
+
 import React from 'react'
 import PDF from '../../assets/Anniel Valle Valera.pdf'
 
 export const Resume = () => {
   return (
     <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
-      <iframe src={PDF} width='100%' height='100%' />
+      <iframe src={PDF} name='demo' width='100%' height='100%' />
     </div>
   )
 }
