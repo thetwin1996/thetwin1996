c2386d22793ea384facd61004a7e1f34a9ac8d9c
TheTwin96 <annielvallevalera96@outlook.com>
2024-01-08 10:46:53 -0600

    Changes.

--- src/elements/button/Button.jsx
+++ src/elements/button/Button.jsx
@@ -3,19 +3,7 @@ import './Button.css'
 import PDF from '../../assets/Anniel Valle Valera.pdf'
 
 export function Button ({ children, isDownload }) {
-  const onButtonClick = () => {
-    fetch(PDF).then((response) => {
-      response.blob().then((blob) => {
-        const fileURL = window.URL.createObjectURL(blob)
-        const alink = document.createElement('a')
-        alink.href = fileURL
-        alink.download = 'Anniel Valle Valera.pdf'
-        alink.click()
-      })
-    })
-  }
-
   return isDownload
-    ? <button id='download-button' className='B_Button' onClick={onButtonClick}>{children}</button>
+    ? <a href={PDF} className='B_Button' download='Anniel Valle Valera.pdf'>{children}</a>
     : <Link to='/resume' className='B_Button' target='_blank'>{children}</Link>
 }
