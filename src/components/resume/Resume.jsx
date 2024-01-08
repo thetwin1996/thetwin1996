19d8b913005a4699118572399595db1fff85833f
TheTwin96 <annielvallevalera96@outlook.com>
2024-01-08 11:27:16 -0600

    Changes.

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,20 @@
+// import PDF from '../../assets/Anniel Valle Valera.pdf'
+
+// export function Resume () {
+//   return (
+//     <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+//       <object data={PDF} type='application/pdf' width='100%' height='99%' />
+//     </div>
+//   )
+// }
+
+import React from 'react'
 import PDF from '../../assets/Anniel Valle Valera.pdf'
 
-export function Resume () {
+export const Resume = () => {
   return (
     <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
-      <object data={PDF} type='application/pdf' width='100%' height='99%' />
+      <iframe src={PDF} width='100%' height='100%' />
     </div>
   )
 }
